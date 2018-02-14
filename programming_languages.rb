294768b2c16b3309dba5ddbc77c5151911eebbb6
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-14 23:40:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,14 @@
 def reformat_languages(languages)
   new_hash = Hash.new
-  new_array = Array.new
-  i = 0
   languages.each do |ooorf, languagesandtypes|
     languagesandtypes.each do |language, type|
         if new_hash.include?(language)
           new_hash[language][:style].push(ooorf)
         elsif
           new_hash[language] = type
-          type[:style] = [types]
-    end 
-    new_hash
+          type[:style] = [ooorf]
+        end
+    end
   end 
   new_hash
 end
